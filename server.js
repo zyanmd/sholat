0bab4be7f5d48141f02da65156c0cb84a1f569b9
Hanz <161605864+zyanmd@users.noreply.github.com>
2025-03-04 10:32:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { jadwalSholat } from './scraper.js'; // Simpan kode scrapper di scraper.js
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware untuk menggunakan static files
 app.use(express.static('public'));
@@ -14,13 +14,19 @@ app.get('/', (req, res) => {
 // Endpoint untuk mengambil jadwal sholat
 app.get('/jadwal/:kota', async (req, res) => {
     const { kota } = req.params;
-    const data = await jadwalSholat(kota);
-    res.json(data);
+    try {
+        const data = await jadwalSholat(kota);
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Gagal mengambil data jadwal sholat' });
+    }
 });
 
 // Jalankan server di Vercel
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-  });
-  
-  export default app;
+if (process.env.NODE_ENV !== 'vercel') {
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+}
+
+export default app;
